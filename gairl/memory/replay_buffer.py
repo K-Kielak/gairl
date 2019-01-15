e867235dedf07d2d60c307986382a123e73e8c33
Kacper Kielak <kielak.kacper@gmail.com>
2019-01-15 13:07:15 +0000

    Create ReplayBuffer class
    ReplayBuffer serves as an experience memory unit for DQN-cognate
    algorithms. Tests for the ReplayBuffer are also included.

---
+++ gairl/memory/replay_buffer.py
@@ -0,0 +1,51 @@
+from collections import deque
+from random import sample
+
+import numpy as np
+
+
+class ReplayBuffer:
+
+    def __init__(self, max_capacity, min_capacity, replay_batch_size):
+        """
+        :param max_capacity: int; maximum number of experience
+            samples that can be stored by the buffer.
+        :param min_capacity: int; the minimum amount of experience
+            samples that are needed to replay the experience.
+        :param replay_batch_size: int; how many samples are returned
+            when replaying the experience.
+        """
+        if replay_batch_size > min_capacity:
+            raise AttributeError(f'min_capacity ({min_capacity}) has '
+                                 f'to be equal or higher than '
+                                 f'replay_batch_size ({replay_batch_size})')
+
+        if min_capacity > max_capacity:
+            raise AttributeError(f'max_capacity ({max_capacity} has'
+                                 f'to be equal or higher than'
+                                 f'min_capacity ({min_capacity})')
+
+        self._max_capacity = max_capacity
+        self._min_capacity = min_capacity
+        self._replay_batch_size = replay_batch_size
+        self._buffer = deque()
+
+    def add_experience(self, start_state, action, reward, next_state):
+        """
+        Takes experience s_{t}, a_{t}, r_{t+1}, s_{t+1} and saves
+        it to the buffer.
+        """
+        self._buffer.appendleft((start_state, action, reward, next_state))
+        if len(self._buffer) > self._max_capacity:
+            self._buffer.pop()
+
+    def replay_experience(self):
+        """
+        :return: self._replay_batch_size randomly sampled experience
+            tuples (s_{t}, a_{t}, r_{t+1}, s_{t+1}) from the buffer.
+        """
+        if len(self._buffer) < self._min_capacity:
+            return None
+
+        samples = sample(self._buffer, self._replay_batch_size)
+        return np.array(samples)
---
+++ tests/memory/test_replay_buffer.py
@@ -0,0 +1,99 @@
+import random
+
+import pytest
+from hamcrest import assert_that, contains, equal_to
+
+from gairl.memory.replay_buffer import ReplayBuffer
+
+
+def test_init_valid1():
+    # When
+    rep_buff = ReplayBuffer(5, 5, 5)
+
+    # Then
+    assert_that(rep_buff._max_capacity, equal_to(5))
+    assert_that(rep_buff._min_capacity, equal_to(5))
+    assert_that(rep_buff._replay_batch_size, equal_to(5))
+
+
+def test_init_valid2():
+    # When
+    rep_buff = ReplayBuffer(7, 5, 2)
+
+    # Then
+    assert_that(rep_buff._max_capacity, equal_to(7))
+    assert_that(rep_buff._min_capacity, equal_to(5))
+    assert_that(rep_buff._replay_batch_size, equal_to(2))
+
+
+def test_init_batch_size_higher_than_min_cap():
+    # When/Then
+    with pytest.raises(AttributeError):
+        ReplayBuffer(7, 5, 6)
+
+
+def test_init_min_cap_higher_than_max_cap():
+    # When/Then
+    with pytest.raises(AttributeError):
+        ReplayBuffer(7, 8, 3)
+
+
+def test_add_experience_max_not_exceeded():
+    # Given
+    rep_buff = ReplayBuffer(4, 2, 2)
+
+    # When
+    rep_buff.add_experience('s1', 'a1', 'r2', 's2')
+    rep_buff.add_experience('s2', 'a2', 'r3', 's3')
+    rep_buff.add_experience('s3', 'a3', 'r4', 's4')
+    rep_buff.add_experience('s4', 'a4', 'r5', 's5')
+
+    # Then
+    assert_that(rep_buff._buffer, contains(
+        ('s4', 'a4', 'r5', 's5'),
+        ('s3', 'a3', 'r4', 's4'),
+        ('s2', 'a2', 'r3', 's3'),
+        ('s1', 'a1', 'r2', 's2'),
+    ))
+
+
+def test_add_experience_max_exceeded():
+    # Given
+    rep_buff = ReplayBuffer(3, 2, 2)
+
+    # When
+    rep_buff.add_experience('s1', 'a1', 'r2', 's2')
+    rep_buff.add_experience('s2', 'a2', 'r3', 's3')
+    rep_buff.add_experience('s3', 'a3', 'r4', 's4')
+    rep_buff.add_experience('s4', 'a4', 'r5', 's5')
+    rep_buff.add_experience('s5', 'a5', 'r6', 's6')
+
+    # Then
+    assert_that(rep_buff._buffer, contains(
+        ('s5', 'a5', 'r6', 's6'),
+        ('s4', 'a4', 'r5', 's5'),
+        ('s3', 'a3', 'r4', 's4')
+    ))
+
+
+def test_replay_experience():
+    # Given
+    random.seed(9812312)
+    rep_buff = ReplayBuffer(3, 2, 2)
+
+    # When
+    rep_buff.add_experience('s1', 'a1', 'r2', 's2')
+    rep_buff.add_experience('s2', 'a2', 'r3', 's3')
+    rep_buff.add_experience('s3', 'a3', 'r4', 's4')
+    rep_buff.add_experience('s4', 'a4', 'r5', 's5')
+    rep_buff.add_experience('s5', 'a5', 'r6', 's6')
+
+    # Then
+    assert_that(rep_buff.replay_experience().tolist(), contains(
+        ['s3', 'a3', 'r4', 's4'],
+        ['s5', 'a5', 'r6', 's6']
+    ))
+    assert_that(rep_buff.replay_experience().tolist(), contains(
+        ['s5', 'a5', 'r6', 's6'],
+        ['s4', 'a4', 'r5', 's5']
+    ))
