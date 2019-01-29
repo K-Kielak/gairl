790fabd823425ba2987418fdc88fd7952a3d5c26
Kacper Kielak <kielak.kacper@gmail.com>
2019-01-29 15:23:03 +0000

    Implement Acrobot experiment runnable

--- gairl/config.py
+++ gairl/config.py
@@ -5,5 +5,5 @@ PROJECT_ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 OUTPUTS_DIR = os.path.join(PROJECT_ROOT_DIR, 'outputs')
 
 AGENT_STR = 'dqn'
-DELAY_BETWEEN_RENDERS = 0.1  # So render can be slowed down [s]
+DELAY_BETWEEN_RENDERS = 0  # So render can be slowed down [s]
 RENDER = True
