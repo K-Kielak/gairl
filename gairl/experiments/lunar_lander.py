df502006a44c83f9136ed8940ba567bea77dc37d
Kacper Kielak <kielak.kacper@gmail.com>
2019-01-29 18:21:25 +0000

    Implement LunarLander environment runnable

---
+++ gairl/experiments/lunar_lander.py
@@ -0,0 +1,35 @@
+import time
+
+import tensorflow as tf
+from gym.envs.box2d import LunarLander
+
+from gairl.agents import create_agent
+from gairl.config import AGENT_STR, DELAY_BETWEEN_RENDERS, RENDER
+
+
+EPISODES_NUM = 5000
+
+
+def main():
+    env = LunarLander()
+    with tf.Session() as sess:
+        agent = create_agent(AGENT_STR, env.action_space.n,
+                             env.observation_space.shape[0], sess)
+        for e in range(EPISODES_NUM):
+            observation = env.reset()
+            action = agent.step(observation)
+            while True:
+                if RENDER:
+                    env.render()
+                    time.sleep(DELAY_BETWEEN_RENDERS)
+
+                observation, reward, done, _ = env.step(action)
+                if done:
+                    agent.step(observation, reward, is_terminal=True)
+                    break
+                else:
+                    action = agent.step(observation, reward)
+
+
+if __name__ == '__main__':
+    main()
