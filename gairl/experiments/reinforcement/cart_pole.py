55637f4a6a58423fda9f16598a499d0947d9d136
Kacper Kielak <kielak.kacper@gmail.com>
2019-03-07 20:41:00 +0000

    Modify RL experiments to pass expected data ranges

--- gairl/experiments/reinforcement/cart_pole.py
+++ gairl/experiments/reinforcement/cart_pole.py
@@ -1,5 +1,6 @@
 import time
 
+import numpy as np
 import tensorflow as tf
 from gym.envs.classic_control import CartPoleEnv
 
@@ -13,8 +14,12 @@ MAX_STEPS_PER_EPISODE = 200
 def main():
     env = CartPoleEnv()
     with tf.Session() as sess:
+        s_ranges = np.concatenate((np.vstack(env.observation_space.low),
+                                   np.vstack(env.observation_space.high)), 1)
+        a_ranges = [[0, 1]] * env.action_space.n
         agent = create_agent(AGENT_STR, env.action_space.n,
-                             env.observation_space.shape[0], sess)
+                             env.observation_space.shape[0], sess,
+                             state_ranges=s_ranges, action_ranges=a_ranges)
         for e in range(EPISODES_NUM):
             observation = env.reset()
             action = agent.step(observation)
