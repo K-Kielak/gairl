25adb615b1b38ff02018b050c79324ee216932e4
Kacper Kielak <kielak.kacper@gmail.com>
2019-03-08 21:43:40 +0000

    Tune MLP parameters for GAIRL

--- gairl/agents/gairl/gairl_config.py
+++ gairl/agents/gairl/gairl_config.py
@@ -7,11 +7,11 @@ from gairl.config import OUTPUTS_DIR
 
 
 OUTPUT_DIRECTORY = os.path.join(OUTPUTS_DIR, 'reinforcement', 'acrobot',
-                                'gairl', 'dqn,mlp-default')
+                                'gairl', '20kf50kg,dqn,mlp16,16d0.75')
 GENERATIVE_MODEL_STR = 'mlp'
 RL_AGENT_STR = 'dqn'
 DTYPE = tf.float64
-MODEL_FREE_STEPS = 50000
+MODEL_FREE_STEPS = 20000
 MODEL_TRAINING_STEPS = 50000
 MODEL_MEMORY_SIZE = 1000000
 MODEL_BATCH_SIZE = 256
--- gairl/generators/mlp/multilayer_perceptron_config.py
+++ gairl/generators/mlp/multilayer_perceptron_config.py
@@ -9,13 +9,13 @@ from gairl.config import OUTPUTS_DIR
 OUTPUT_DIRECTORY = os.path.join(OUTPUTS_DIR, 'generation', 'cond_mnist',
                                 'mlp', 'test')
 DTYPE = tf.float64
-LAYERS = (8,)
+LAYERS = (16, 16)
 ACTIVATION = tf.nn.leaky_relu
 FINAL_ACTIVATION = None
 NORM_RANGE = (0, 1)
-DROPOUT = 0.5
+DROPOUT = 0.75
 OPTIMIZER = tf.train.AdamOptimizer(learning_rate=2e-4)
-LOGGING_FREQ = 5000
+LOGGING_FREQ = 50000
 LOGGING_LEVEL = logging.INFO
 MAX_CHECKPOINTS = 5
 SAVE_FREQ = 1000
